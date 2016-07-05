afb970ca9acac0ebaeb8243b6a3ad5c7210daa6b
Benjamin Dupuis <benjamin.dupuis@fr.clara.net>
2016-07-05 19:04:10 +0200

    Fix validate

--- manifests/install.pp
+++ manifests/install.pp
@@ -1,14 +1,19 @@
 # == define php::install
 define php::install (
-  $ensure_cli = true,
+  $ensure_cli = present,
   $cli_conf = {},
-  $ensure_mod_php = false,
+  $ensure_mod_php = absent,
   $mod_php_conf = {},
   $ensure_fpm = absent,
   $fpm_conf = {},
   $fpm_pool = {},
   $modules = {},
 ) {
+
+  validate_re($ensure_cli, '^(present)|(installed)|(absent)$', "ensure_cli is '${ensure_cli}' and must be absent, present or installed")
+  validate_re($ensure_mod_php, '^(present)|(installed)|(absent)$', "ensure_cli is '${ensure_cli}' and must be absent, present or installed")
+  validate_re($ensure_fpm, '^(present)|(installed)|(absent)$', "ensure_fpm, is '${ensure_cli}' and must be absent, present or installed")
+
   php::fpm::install { $name:
     ensure  => $ensure_fpm,
   }
