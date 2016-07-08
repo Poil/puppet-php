30e53453f91f7d9c2298505043f8907c8b29ad7b
Benjamin Dupuis <benjamin.dupuis@fr.clara.net>
2016-07-08 22:58:14 +0200

    Manage on ALL

--- manifests/extension/sapi.pp
+++ manifests/extension/sapi.pp
@@ -7,19 +7,41 @@ define php::extension::sapi (
 ) {
   $sapi_tmp = split($name, '/')
   $sapi = $sapi_tmp[2]
-  case $ensure {
-    'absent' : {
-      exec { "${ext_tool_disable} -m ${module} -s ${sapi}":
-        onlyif  => "${ext_tool_query} -s ${sapi} -m ${module} | grep Enabled",
-      }
-    }
-    'present' : {
-      exec { "${ext_tool_enable} -m ${module} -s ${sapi}":
-        unless  => "${ext_tool_query} -s ${sapi} -m ${module} | grep Enabled",
+
+  case $sapi {
+    'ALL' : {
+      case $ensure {
+        'absent' : {
+          exec { "${ext_tool_query} -S | xargs -I {} ${ext_tool_disable} -s {} -m ${module}":
+            onlyif  => "${ext_tool_query} -S | xargs -I {} ${ext_tool_query} -s {} -m ${module} | grep Enabled",
+          }
+        }
+        'present' : {
+          exec { "${ext_tool_query} -S | xargs -I {} ${ext_tool_enable} -s {} -m ${module}":
+            unless => "${ext_tool_query} -S | xargs -I {} ${ext_tool_query} -s {} -m ${module} | grep Enabled",
+          }
+        }
+        default : {
+          fail("Error - ${module_name}, unknown ensure value ${ensure}")
+        }
       }
     }
     default : {
-      fail("Error - ${module_name}, unknown ensure value ${ensure}")
+      case $ensure {
+        'absent' : {
+          exec { "${ext_tool_disable} -s ${sapi} -m ${module}":
+            onlyif  => "${ext_tool_query} -s ${sapi} -m ${module} | grep Enabled",
+          }
+        }
+        'present' : {
+          exec { "${ext_tool_enable} -s ${sapi} -m ${module}":
+            unless  => "${ext_tool_query} -s ${sapi} -m ${module} | grep Enabled",
+          }
+        }
+        default : {
+          fail("Error - ${module_name}, unknown ensure value ${ensure}")
+        }
+      }
     }
   }
 }
