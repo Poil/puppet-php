44895e883a0e8cea92195ae917f194e6c4416a1f
Benjamin Dupuis <benjamin.dupuis@fr.clara.net>
2016-07-11 15:13:35 +0200

    Package prefix

--- manifests/extension.pp
+++ manifests/extension.pp
@@ -3,14 +3,16 @@ define php::extension (
   $ensure,
   $php_version,
   $sapi = [],
+  $package_prefix = undef,
 ) {
 
   case $::operatingsystem {
     'Ubuntu': {
       ::php::extension::ubuntu { $name:
-        ensure      => $ensure,
-        php_version => $php_version,
-        sapi        => $sapi,
+        ensure         => $ensure,
+        php_version    => $php_version,
+        sapi           => $sapi,
+        package_prefix => $package_prefix,
       }
     }
     default: {
