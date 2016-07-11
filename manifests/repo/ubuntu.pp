3b42e6c4f481d13e21ca83eda8a27c85824d1ddf
Benjamin Dupuis <benjamin.dupuis@fr.clara.net>
2016-07-11 13:55:22 +0200

    Remove unused control

--- manifests/repo/ubuntu.pp
+++ manifests/repo/ubuntu.pp
@@ -13,9 +13,6 @@ class php::repo::ubuntu (
   }
 
   $versions_keys = keys($::php::versions)
-  if intersection($versions_keys, ['5.6' , '5.7', '7.0', '7.1']) == $versions_keys {
-    fail("Error - ${module_name} versions ${versions_keys} are not supported by ondrej repository")
-  }
 
   include '::apt'
 
