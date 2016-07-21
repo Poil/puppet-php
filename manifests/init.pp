10178c1700c509aaa7bd67f45aaa8ce460df1453
Benjamin Dupuis <benjamin.dupuis@fr.clara.net>
2016-07-21 16:53:59 +0200

    Add redhat validation

--- manifests/init.pp
+++ manifests/init.pp
@@ -27,18 +27,31 @@ class php (
     }
     'Ubuntu': {
       if count(keys($versions)) > 1 and $repo == 'distrib'  {
-        fail("error - ${module_name} : on ${::operatingsystem} you must set repo to ondrej to support multiple php version")
+        fail("Error - ${module_name} : On ${::operatingsystem} you must set repo to ondrej to support multiple php version")
       }
       if $repo == 'ondrej' {
         $versions_keys = keys($versions)
         if count(intersection($versions_keys, ['5.6' , '7.0', '7.1'])) != count($versions_keys) {
-          fail("Error - ${module_name} versions ${versions_keys} are not supported by ondrej repository")
+          fail("Error - ${module_name} : Versions ${versions_keys} are not supported by ondrej repository")
         }
       }
     }
     'RedHat', 'CentOS', 'OracleLinux': {
       if count(keys($versions)) > 1 and $repo == 'distrib'  {
-        fail("error - ${module_name} : on ${::operatingsystem} you must set repo to scl to support multiple php version")
+        fail("Error - ${module_name} : On ${::operatingsystem} you must set repo to scl to support multiple php version")
+      }
+      if $repo == 'scl' {
+        case $::operatingsystemmajrelease {
+          '6', '7': {
+            $versions_keys = keys($versions)
+            if count(intersection($versions_keys, ['5.4' , '5.5', '5.6'])) != count($versions_keys) {
+              fail("Error - ${module_name} versions ${versions_keys} are not supported by scl repository")
+            }
+          }
+          default : {
+            fail("Error - ${module_name} : SCL repo is unsupported on ${::operatingsystem} ${::operatingsystemmajrelease}")
+          }
+        }
       }
     }
     default: {
