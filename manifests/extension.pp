e5c919fd12c53e84eafd45c8d884b9da6a26f425
Benjamin Dupuis <benjamin.dupuis@fr.clara.net>
2016-07-21 10:38:48 +0200

    Missing debian

--- manifests/extension.pp
+++ manifests/extension.pp
@@ -17,6 +17,15 @@ define php::extension (
         package_prefix   => $package_prefix,
       }
     }
+    'Debian': {
+      ::php::extension::debian { $name:
+        ensure           => $ensure,
+        php_version      => $php_version,
+        sapi             => $sapi,
+        extension_config => $extension_config,
+        package_prefix   => $package_prefix,
+      }
+    }
     default: {
       fail("Error - ${module_name}, unsupported OS ${::operatingsytem}")
     }
