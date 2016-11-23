475133bd50b43fc5b2fc6b515cba5bb757030dfb
Benjamin Dupuis <benjamin.dupuis@fr.clara.net>
2016-11-23 10:19:05 +0100

    Better code for socket path determination

--- manifests/fpm/pool.pp
+++ manifests/fpm/pool.pp
@@ -34,16 +34,11 @@ define php::fpm::pool (
   }
 
   # We always use the path from the repo, there is no way to determine if the package is from distrib or from repo if repo is declared
-  if $::php::repo == 'scl' {
-    if has_key($::php::fpm_socket_dir[$os][$::operatingsystemmajrelease][$::php::repo], $version) {
-      $default_socket_dir = $::php::fpm_socket_dir[$os][$::operatingsystemmajrelease][$::php::repo][$version]
-      $_listen = pick($listen, "${default_socket_dir}/php${version}-fpm.${pool_name}.sock")
-    } else {
-      $default_socket_dir = $::php::fpm_socket_dir[$os][$::operatingsystemmajrelease][$::php::repo]
-      $_listen = pick($listen, "${default_socket_dir}/php${version}-fpm.${pool_name}.sock")
-    }
+  if has_key($::php::fpm_socket_dir[$os][$::operatingsystemmajrelease][$::php::repo], $version) {
+    $default_socket_dir = $::php::fpm_socket_dir[$os][$::operatingsystemmajrelease][$::php::repo][$version]
+    $_listen = pick($listen, "${default_socket_dir}/php${version}-fpm.${pool_name}.sock")
   } else {
-    $default_socket_dir = $::php::fpm_socket_dir[$os][$::operatingsystemmajrelease][$::php::repo]
+    $default_socket_dir = $::php::fpm_socket_dir[$os][$::operatingsystemmajrelease][$::php::repo]['default']
     $_listen = pick($listen, "${default_socket_dir}/php${version}-fpm.${pool_name}.sock")
   }
 
