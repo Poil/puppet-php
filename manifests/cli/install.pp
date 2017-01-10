244ecba02dc51474b178d5c71de2f1994186d345
Benjamin Dupuis <benjamin.dupuis@fr.clara.net>
2017-01-10 10:22:31 +0100

    Add default memory limit to -1 for cli

--- manifests/cli/install.pp
+++ manifests/cli/install.pp
@@ -3,23 +3,28 @@ define php::cli::install (
   $ensure = 'present',
   $custom_config = {},
 ) {
+  $default_config = {
+    'PHP' => { 'memory_limit' => '-1' }
+  }
+  $custom_cli_config = deep_merge($default_config, $custom_config)
+
   case $::operatingsystem {
     'Ubuntu': {
       ::php::cli::install::ubuntu { $name :
         ensure         => $ensure,
-        custom_config  => $custom_config,
+        custom_config  => $custom_cli_config,
       }
     }
     'Debian': {
       ::php::cli::install::debian { $name :
         ensure        => $ensure,
-        custom_config => $custom_config,
+        custom_config => $custom_cli_config,
       }
     }
     'RedHat', 'CentOS','OracleLinux': {
       ::php::cli::install::redhat { $name :
         ensure        => $ensure,
-        custom_config => $custom_config,
+        custom_config => $custom_cli_config,
       }
     }
     default: {
